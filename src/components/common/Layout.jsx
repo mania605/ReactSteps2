dad90213c9005d6181c1bd31e9ad293c1af855ff
mania605 <kim.minsun83@gmail.com>
2024-10-02 11:13:50 +0900

    배열 데이터를 통한 반복 JSX처리

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,10 +1,9 @@
 export default function Layout({ title, children }) {
 	return (
-		<main className={title}>
+		<main className={title.toLowerCase()}>
 			<h1>{title}</h1>
+
 			<section>{children}</section>
 		</main>
 	);
 }
-//위의 레이아웃 컴포넌트 구조 분석
-//모든 서브페이지 컴포넌트에 위의 레이아웃 컴포넌트를 적용해서 같은 레이아웃을 공유하도록 설정
