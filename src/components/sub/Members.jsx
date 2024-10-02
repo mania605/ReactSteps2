c4c95912f091cb114d76fa298d004f94b2f940ba
mania605 <kim.minsun83@gmail.com>
2024-10-02 14:22:22 +0900

    멤버데이터반복출력

--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -1,11 +1,20 @@
 import Layout from '../common/Layout';
+import memberData from '../../data/memberData';
 
 export default function Members() {
+	console.log(memberData);
 	return (
-		<main className='members'>
-			<Layout title={'MEMBERS'}>
-				<p>Home Page contents come here.</p>
-			</Layout>
-		</main>
+		<Layout title={'MEMBERS'}>
+			<p>Members Page contents come here.</p>
+			{/* <article>
+				<div className="pic">
+					<img src="이미지" alt="" />
+				</div>
+				<div className="txt">
+					<h2>사람 이름</h2>
+					<p>직책</p>
+				</div>
+			</article> */}
+		</Layout>
 	);
 }
