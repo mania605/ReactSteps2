799e057e3ad22704013515338f9981637f15e137
mania605 <kim.minsun83@gmail.com>
2024-10-02 15:54:42 +0900

    멤버 페이지 스타일링 중간 단계

--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -1,12 +1,9 @@
-//미션 위의 7개 배열 중에서 첫번째 데이터만 뽑아서 기존 ceoBox안쪽으로 출력하고 첫번째를 제외한 나머지 6개 데이타만 기존 반복문구안에서 출력
-
 import Layout from '../common/Layout';
 import memberData from '../../data/memberData';
 
 export default function Members() {
 	return (
 		<Layout title={'MEMBERS'}>
-			{/* 첫번째 데이터만 뽑아서 출력 */}
 			<article className='ceoBox'>
 				<div className='txt'>
 					<h2>{memberData[0].name}</h2>
@@ -16,10 +13,17 @@ export default function Members() {
 					<img src={'/' + memberData[0].pic} alt={memberData[0].name} />
 				</div>
 			</article>
-			<article className='memberListbox'>
+
+			<article className='memberListBox'>
+				<div className='titBox'>
+					<h2>Our Team Members</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora possimus non ipsa cum. Veritatis, dolore
+						aliquam? Consectetur assumenda dolor labore.
+					</p>
+				</div>
 				<ul>
 					{memberData.map((member, idx) => {
-						//첫번째 순번의 데이터가 아닐때에만 반복출력
 						if (idx !== 0) {
 							return (
 								<li key={idx}>
@@ -35,6 +39,14 @@ export default function Members() {
 						}
 					})}
 				</ul>
+				<div className='descBox'>
+					<h2>Lorem ipsum dolor sit.</h2>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. A esse cupiditate, vitae deleniti repellat
+						explicabo sit, corrupti beatae dicta, nulla optio corporis alias. Perferendis quidem sapiente minima,
+						quisquam inventore soluta.
+					</p>
+				</div>
 			</article>
 		</Layout>
 	);
