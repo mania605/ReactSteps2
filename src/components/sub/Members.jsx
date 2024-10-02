c60e8186c85304591923c79070da3913e05a2ca0
mania605 <kim.minsun83@gmail.com>
2024-10-02 14:41:46 +0900

    멤버 데이터 사진포함 반복 출력 패턴익힐것

--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -1,45 +1,23 @@
-{
-	/*
 import Layout from '../common/Layout';
 import memberData from '../../data/memberData';
 export default function Members() {
 	console.log(memberData);
 	return (
 		<Layout title={'MEMBERS'}>
-			<p>Members Page contents come here.</p>
-			{/* <article>
-				<div className="pic">
-					<img src="이미지" alt="" />
-				</div>
-				<div className="txt">
-					<h2>사람 이름</h2>
-					<p>직책</p>
-				</div>
-			</article> 
-		</Layout>);
-}
-*/
-}
+			{memberData.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<div className='pic'>
+							<img src={'/' + member.pic} alt={member.name} />
+						</div>
 
-import Layout from '../common/Layout';
-import memberData from '../../data/memberData';
-
-export default function Members() {
-	console.log(memberData);
-	return (
-		<Layout title={'MEMBERS'}>
-			<p>Members Page contents come here.</p>
-			{memberData.map((member, idx) => (
-				<article key={idx}>
-					<div className='pic'>
-						<img src={`/public/${member.pic}`} alt={member.name} />
-					</div>
-					<div className='txt'>
-						<h2>{member.name}</h2>
-						<p>{member.position}</p>
-					</div>
-				</article>
-			))}
+						<div className='txt'>
+							<h2> {member.name} </h2>
+							<p>{member.position}</p>
+						</div>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
