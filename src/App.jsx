25459e9c8a61d3ef9794d807fe26730283535cb3
mania605 <kim.minsun83@gmail.com>
2024-10-04 15:29:43 +0900

    다이나믹 라우터를 통한 특정 정보값 params로 전달하기

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Gallery from './components/sub/Gallery';
 import Members from './components/sub/Members';
 import Posts from './components/sub/Posts';
 import Youtube from './components/sub/Youtube';
+import YoutubeDetail from './components/sub/YoutubeDetail';
 import Contact from './components/sub/Contact';
 import { Route, Routes } from 'react-router-dom';
+import YoutubeDetail from './components/sub/YoutubeDetail';
 
 export default function App() {
 	return (
@@ -18,6 +20,7 @@ export default function App() {
 				<Route path='/members' element={<Members />} />
 				<Route path='/gallery' element={<Gallery />} />
 				<Route path='/youtube' element={<Youtube />} />
+				<Route path='/youtube/:id' element={<YoutubeDetail />} />
 				<Route path='/contact' element={<Contact />} />
 				<Route path='/posts' element={<Posts />} />
 			</Routes>
@@ -26,6 +29,12 @@ export default function App() {
 		</>
 	);
 }
+
+/*
+	다이나믹 라우터 (상세페이지 제작)
+	-라우터 연결시 path경로를 /패스명/:파라미터
+	-특정 url경로 접속시 파라미터명으로 특정 값 전달
+*/
 
 /*
 	SSR방식 (HTML파일 불러오는 방식)
