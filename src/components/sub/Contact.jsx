988924c65a772c30d64e56debed67ff66dea503e
mania605 <kim.minsun83@gmail.com>
2024-10-04 17:21:10 +0900

    useState, useEffect를 활용한 서버 데이터 fetching후 화면에 반복출력하는 패턴 문법(암기 필수)

--- src/components/sub/Contact.jsx
+++ src/components/sub/Contact.jsx
@@ -1,11 +1,39 @@
 import Layout from '../common/Layout';
+import { useState, useEffect } from 'react';
 
 export default function Contact() {
+	//순서1: 서버쪽에서 받아올 데이터를 담을 빈 배열 state공간및 함수 생성
+	const [Data, setData] = useState([]);
+	//순서2: 의존성배열이 비어있는 useEffect코드
+	useEffect(() => {
+		fetch('/data.json')
+			.then(data => data.json())
+			.then(json => {
+				console.log(json);
+				//순서3: 서버에서 가져온 데이터에서 배열만 뽑은 뒤 무조건 state에 옮겨담기
+				setData(json.data);
+			});
+	}, []);
 	return (
-		<main className='contact'>
-			<Layout title={'CONTACT'}>
-				<p>Home Page contents come here.</p>
-			</Layout>
-		</main>
+		<Layout title={'CONTACT'}>
+			{Data.map((data, idx) => {
+				return (
+					// 순서4:반복시 무조건 idx로 키값 설정
+					<article key={idx}>
+						<h2>{data.name}</h2>
+					</article>
+				);
+			})}
+		</Layout>
 	);
 }
+/*
+자가진단 항목
+1. useState를 이용해서 state에 값을 옮겨담고 state변경함수로 state값 변경처리
+2. useEffect구문의 구조를 파악하고 의존성 배열의 역할
+3. useState와 useEffect를 활용해서 서버 데이터 fetching처리후 state에 담기
+4. 커스텀훅 만드는 방법 (커스텀훅의 조건 및 호출 위치)
+5. 다이나믹 라우터를 이용해서  /youtube/:id를 활용해서 상세페이지에 특정 id값 전달하는 방법
+
+
+*/
