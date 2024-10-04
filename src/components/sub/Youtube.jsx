2a514696daea4b2dfefde523e3ebee672999dc80
mania605 <kim.minsun83@gmail.com>
2024-10-04 13:05:38 +0900

    코드 정리 fetch 모아놓기

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -5,21 +5,21 @@ import Pic from '../common/Pic';
 export default function Youtube() {
 	const [Vids, setVids] = useState([]);
 
-	const api_key = 'AIzaSyCtJt2jnOcXV6eLUZmF2gT6LGa3mSPkpbM';
-	const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
-	const num = 10;
-	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
+	const fetchYoutube = () => {
+		const api_key = 'AIzaSyCtJt2jnOcXV6eLUZmF2gT6LGa3mSPkpbM';
+		const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+		const num = 10;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
 
-	//미션-
-	//2. 유튜브 섬네일 제목 본문 날짜까지 출력 pic component활용
-
-	useEffect(() => {
 		fetch(url)
 			.then(data => data.json())
 			.then(json => {
-				console.log(json.items);
 				setVids(json.items);
 			});
+	};
+
+	useEffect(() => {
+		fetchYoutube();
 	}, []);
 
 	return (
