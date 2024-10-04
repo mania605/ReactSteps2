4cc22a4d62078c9f436ff2fcfe642b7dafae5452
mania605 <kim.minsun83@gmail.com>
2024-10-04 10:41:00 +0900

    useEffect사용시 무한로딩 상황

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -2,28 +2,24 @@ import { useState, useEffect } from 'react';
 import Layout from '../common/Layout';
 
 export default function Youtube() {
-	const [Num, setNum] = useState(0);
+	const [Vids, setVids] = useState([]);
+	console.log(Vids);
 
-	useEffect(() => {
-		//의존성배열이 비어있는 useEffect안쪽의 콜백함수는 컴포넌트 마운트시 한번만 호출됨
-		console.log('Mounted');
-		return () => {
-			//clean-up함수 해당 컴포넌트 언마운트시 실행될 함수
-			console.log('UnMounted');
-		};
-	}, []);
-	useEffect(() => {
-		//의존성배열에 등록된 Num값이 변경될때마다 useEffect안쪽의 콜백함수 호출됨
-		console.log('Num값 변경될때마다 호출');
-	}, [Num]);
+	const api_key = 'AIzaSyDfF904vE_uzyNlnhKgAyUmNWV9U5vTxZ0';
+	const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+	const num = 10;
+	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
 
-	return (
-		<Layout title={'YOUTUBE'}>
-			<h2>{Num}</h2>
-			<button onClick={() => setNum(Num + 1)}>changeNum</button>
-		</Layout>
-	);
-}
-/*
-	useEffect (생명주기 관리함수 : 생성(Mount), 변경(ReRender), 소멸(UnMount))
+	/*아래와 같이 서버에서 가져온 데이터를 컴포넌트 안쪽에서 활용하기 위해state담는 구문을 useEffect를 사용하지 않고 바로 활용하면 데이터를 무한호출하는 이슈발생
+	미션 아래 코드 호춢해서 무한데이터 호출 발생하는 현상 확인한 뒤, 위와 같은 이슈 발생하는 원인 고민
+
+	fetch(url)
+		.then(data => data.json())
+		.then(json => {
+			const youtubeArr = json.items;
+			console.log(youtubeArr);
+		});
 */
+	return;
+	<Layout title={'YOUTUBE'}> {}</Layout>;
+}
