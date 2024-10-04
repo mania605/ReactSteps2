7382fd665d212beb6f512302901266b9b3892f5b
mania605 <kim.minsun83@gmail.com>
2024-10-04 10:05:57 +0900

    참조형 자료를 State에 담고 변경할때 주의사항

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -4,12 +4,19 @@ import Layout from '../common/Layout';
 export default function Youtube() {
 	const [Colors, setColors] = useState(['red', 'green', 'blue']);
 
+	//ㄴㅅㅁㅅㄷrkqtdms
+	const changeColor = () => {
+		const newColors = [...Colors];
+		newColors[0] = 'hotpink';
+		setColors(newColors);
+	};
+
 	//console.log();
 
 	return (
 		<Layout title={'YOUTUBE'}>
+			<button onClick={changeColor}>set New Color</button>
 			<ul>
-				{' '}
 				{Colors.map((color, idx) => {
 					return (
 						<li style={{ color: color }} key={idx}>
