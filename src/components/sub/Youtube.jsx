d8318e106155ab7993384cbd3ebc7d5d30edb6ef
mania605 <kim.minsun83@gmail.com>
2024-10-04 13:49:16 +0900

    커스텀 훅 제작 방법 주석 정리

--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react';
 import Layout from '../common/Layout';
 import Pic from '../common/Pic';
+import useShorthenText from '../../../hooks/useShortenText';
 
 export default function Youtube() {
+	const shorthenText = useShorthenText(); //여기서만 호출 가능하지 fetch나 jsx구문 안쪽에서 호룿ㄹ 불가
+	console.log(shorthenText);
 	const [Vids, setVids] = useState([]);
 
 	const fetchYoutube = () => {
+		shortenText('David');
 		const api_key = import.meta.env.VITE_YOUTUBE_API;
 		const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
 		const num = 10;
@@ -29,7 +33,11 @@ export default function Youtube() {
 					<article key={idx}>
 						<Pic className='thumb' src={vid.snippet.thumbnails.high.url} />
 						<h3>{vid.snippet.title.length >= 60 ? vid.snippet.title.substr(0, 60) + '...' : vid.snippet.title}</h3>
-						<p>{vid.snippet.description}</p>
+						<p>
+							{vid.snippet.description.length >> 100
+								? vid.snippet.description.substr(0, 100) + '...'
+								: vid.snippet.description}
+						</p>
 						<span>{vid.snippet.publishedAt}</span>
 					</article>
 				);
