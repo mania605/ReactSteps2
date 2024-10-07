6e4497890a5156aaec3e9ab25d7b317ebc8b28e7
mania605 <kim.minsun83@gmail.com>
2024-10-07 15:59:03 +0900

    목록 클릭시 모달창에 큰 이미지 출력

--- src/components/common/modal.jsx
+++ src/components/common/modal.jsx
@@ -2,6 +2,7 @@ export default function Modal({ children, setModalOpen }) {
 	return (
 		<aside className='modal'>
 			<div className='con'>{children}</div>
+
 			<button className='btnClose' onClick={() => setModalOpen(false)}>
 				CLOSE
 			</button>
--- src/components/sub/Gallery.jsx
+++ src/components/sub/Gallery.jsx
@@ -5,9 +5,9 @@ import Modal from '../common/modal';
 
 export default function Gallery() {
 	const [Flickr, setFlickr] = useState([]);
-	const [ModalOpen, setModalOpen] = useState(false); // 모달 표시 여부를 제어하는 상태
-
 	console.log(Flickr); //1. 외부데이터를 담을 빈 State공간 상태값과 State변경함수를 useState로부터 생성- []로 빈그릇만 만들어 놓는 것
+	const [ModalOpen, setModalOpen] = useState(false); // 모달 표시 여부를 제어하는 상태
+	const [Index, setIndex] = useState(0); //클릭한 목록요소의 순번을 담을 상태값 생성
 
 	useEffect(() => {
 		const method = 'flickr.people.getPhotos';
@@ -41,7 +41,13 @@ export default function Gallery() {
 				<section className='galleryList'>
 					{Flickr.map((data, idx) => {
 						return (
-							<article key={idx} onClick={() => setModalOpen(true)}>
+							<article
+								key={idx}
+								onClick={() => {
+									setModalOpen(true);
+									//각 이미지 목록 클릭시 클릭한 idx순번값을 Index상태값에 저장
+									setIndex(idx);
+								}}>
 								<Pic
 									src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_z.jpg`}
 									className='pic'
@@ -54,31 +60,22 @@ export default function Gallery() {
 				</section>
 			</Layout>
 
-			{ModalOpen && <Modal setModalOpen={setModalOpen}>FLICKR IMAGE</Modal>}
+			{ModalOpen && (
+				<Modal setModalOpen={setModalOpen}>
+					<Pic
+						//Pic컴포넌트 src값으로 Flickr전체 배열에서 Index상태 순번의 정보값으로 _b 접미사의 큰 이미지 주소를 Pic에 전달해서 호출
+						src={`https://live.staticflickr.com/${Flickr[Index].server}/${Flickr[Index].id}_${Flickr[Index].secret}_b.jpg`}
+						shadow
+					/>
+				</Modal>
+			)}
 		</>
 	);
 }
 /*
-부모에세 자식 컴포넌트에 상태변경함수를 전달해서 자식컴포넌트가 부모컴포넌트의 상태값 제어가능하게 처리
-
-*/
-
-/*
-아래만 알아도 React 70%에 해당하는 로직을 이해할 수 있음.
-
-useSTate, useEffect 훅을 이용해서 외부 서버데이터를 가져오고 컴포넌트에 렌더링 하는 패턴
-1. 외부데이터를 담을 빈 State공간 상태값과 State변경함수를 useState로부터 생성
-2. 의존성 배열이 비어있는 useEffect구문 생성(서버데이터는 컴포넌트 초기 렌더링시 한번만 가져오는 것이 일반적)
-3. useEffect구문 안쪽에서 데이터를 요청URL을 생성하기 위한 정보값 변수에 담기
-4. useEffect구문 안쪽에서 완성된 요청URL로 fetch함수를 통해 데이터 요청
-	4-1.만약 제대로 url요청을 했음에도 불구하고 콘솔에러로 'not Valid JSON'에러 뜰시 다음의 쿼리스트링 옵션을 뒤에 추가
-	4-2. nojsoncallback=1&format=json;
-
-5. fetch함수의 then구문 안에서 전달받은 서버데이터로부터 배열만 뽑아서 미리 준비해 놓은 State에 State변경함수로 담기
-6. Return문 안쪽에서 State값을 map으로 반복돌며 원하는 형태의 JSX로 출력
-
-
-미션: 
-위 패턴을 확인하며 Flickr API로부터 내 갤러리 데이터를 화면에 출력 시도
-
+	모달안에 반복 이벤트가 발생한 순번의 요소의 정보를 출력하는 패턴
+	
+	1. 순서값을 저장할 상태값 생성
+	2. 반복 요소에 이벤트 발생시 이벤트가 발생한 요소의 순서값을 상태값에 저장
+	3. 모달 안쪽에서 출력해야되는 정보를 순서 상태값에 연동처리
 */
