e24b72afb42f69c928a02202ac154584db3e4973
mania605 <kim.minsun83@gmail.com>
2024-10-08 11:35:36 +0900

    문자열 분리 커스텀훅 사용

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,17 +1,31 @@
 import { useLocation } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import useSplitText from '../../hooks/useSplitText';
+import { useEffect, useRef } from 'react';
 
 export default function Layout({ title, children }) {
+	const ref_title = useRef(null);
+	//커스텀훅으로 핸들러함수 안쪽에서 호출할 수 있는 실제사용가능한 함수 반환받음
+	const splitText = useSplitText();
 	const { pathname } = useLocation();
 	const isDetail = pathname.includes('/youtube/');
 
+	useEffect(() => {
+		splitText(ref_title);
+	}, []);
 	return (
 		<main className={isDetail ? 'detail' : title.toLowerCase()}>
-			<motion.h1 initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 1 }} exit={{ scale: 2, opacity: 0, transition: { duration: 0.3 } }} transition={{ duration: 1, ease: 'easeIn' }}>
-				{title}
-			</motion.h1>
+			<h1 ref={ref_title}>{title}</h1>
 
 			<section>{children}</section>
 		</main>
 	);
 }
+
+/*
+	motion 컴포넌트에서 자주 쓰는 스타일 속성
+	x: 가로축 이동 (숫자, 퍼센트는 문자열 처리)
+	y: 세로축 이동
+	scale:확대
+	rotate: 회전
+	opacity: 투명도
+*/
--- src/hooks/useSplitText.js
+++ src/hooks/useSplitText.js
@@ -6,6 +6,18 @@
 //Lexical Scope환경이 설정됨.
 //Lexical Scope환경이란
 //안쪽의 지역변수가 계속 값이 유지되는 현상
+
 export default function UseSplitText() {
-	return () => {};
+	//내부적으로 current값을 직접 추출하기 때문에 호출시 참조객체명만 전달
+	return ref => {
+		//인수로 전달받은 참조객체안의 요소의 텍스트만 가져옴
+		let text = ref.current.innerText; // 동적으로 생성될 태그문자열이 담길 빈 변수 생성
+		let tags = ''; //빈문자열 초기화
+		for (let letter of text) {
+			//문자열 반복돌면서 동적으로 <span>요소로 감싸면서 문자열 쌓아나감
+			tags += '<span>${letter}</span>';
+		}
+
+		ref.current.innerHTML = tags; //tag문자열이 완성되면 ref참조 요소 안쪽에 변경된 문자열 DOM구조 바꿔치기
+	};
 }
