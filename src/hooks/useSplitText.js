49a7a49bc923ff34ae4a05c87e70f4a99920a13e
mania605 <kim.minsun83@gmail.com>
2024-10-08 14:13:45 +0900

    함수의 복잡한 구조의 인수 전달방법

--- src/hooks/useSplitText.js
+++ src/hooks/useSplitText.js
@@ -1,12 +1,16 @@
 export default function useSplitText() {
-	return (ref, interval = 0, delay = 0) => {
-		// 세 번째 파라미터 추가 (delay)
+	return (ref, option) => {
+		if (!ref) return console.error('첫번째 인수로는 참조객체값이 와야 됩니다.');
+		const default_option = { interval: 0, delay: 0 };
+		//불변성을 유지하면서 2개의 객체를 합쳐서 새로운 객체 반환하는 패턴
+		//{...객체1, ...객체2}
+		const result_option = { ...default_option, ...option };
 		let text = ref.current.innerText;
 		let tags = '';
 		let count = 0;
 
 		for (let letter of text) {
-			tags += `<span style='display:inline-block; transition-duration:0.5s; transition-delay:${count * interval + delay}s'>${letter}</span>`;
+			tags += `<span style='display:inline-block; transition-duration:0.5s; transition-delay:${count * result_option.interval + result_option.delay}s'>${letter}</span>`;
 			count++;
 		}
 
@@ -14,6 +18,7 @@ export default function useSplitText() {
 
 		setTimeout(() => {
 			ref.current.classList.add('on');
-		}, delay); // delay로 모션 시작 시점을 제어
+		}, 100);
 	};
 }
+//미션 - 위와 같이 함수를 제작시 문제점
