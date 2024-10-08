48a5d25446b36b6126be51904151d1e593a2facf
mania605 <kim.minsun83@gmail.com>
2024-10-08 15:24:31 +0900

    프레임 스타일

--- src/components/common/MaskText.jsx
+++ src/components/common/MaskText.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useRef } from 'react';
 export default function MaskText({ children }) {
 	const ref_frame = useRef(null);
+
+	const frameStyle = { fontSize: '1.2rem', fontFamily: 'orbitron', color: '#555', display: 'inline-block', position: 'relative', overflow: 'hidden', marginBottom: 80 };
+
 	useEffect(() => {
 		setTimeout(() => {
 			ref_frame.current.classList.add('on');
 		}, 100);
 	}, []);
+
 	return (
-		<div className='slogan' ref={ref_frame}>
+		<div className='slogan' style={frameStyle} ref={ref_frame}>
 			<span>{children}</span>
 			<div className='mask'></div>
 		</div>
