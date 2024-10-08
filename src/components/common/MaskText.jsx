3d9a091deb08d738d1006a1c59b0f3039243ef02
mania605 <kim.minsun83@gmail.com>
2024-10-08 15:48:19 +0900

    MaskText 스타일 객체 연결(오류버전)

--- src/components/common/MaskText.jsx
+++ src/components/common/MaskText.jsx
@@ -1,15 +1,25 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
+
 export default function MaskText({ children }) {
-	const ref_frame = useRef(null);
 	const [Mounted, setMounted] = useState(false);
 
-	const frameStyle = { fontSize: '1.2rem', fontFamily: 'orbitron', color: '#555', display: 'inline-block', position: 'relative', overflow: 'hidden', marginBottom: 80 };
-
-	const conStyle = { opacity: 0, transitionDuration: '0.1s', transitionDelay: '0.3s' };
-
+	const frameStyle = {
+		fontSize: '1.2rem',
+		fontFamily: 'orbitron',
+		color: '#555',
+		display: 'inline-block',
+		position: 'relative',
+		overflow: 'hidden',
+		marginBottom: 80
+	};
+	const conStyle = {
+		opacity: 0,
+		transitionDuration: '0.1s',
+		transitionDelay: '0.3s'
+	};
 	const maskStyle = {
 		width: '100%',
-		height: '100%',
+		heigiht: '100%',
 		position: 'absolute',
 		top: 0,
 		left: '-100%',
@@ -18,19 +28,20 @@ export default function MaskText({ children }) {
 		transitionDuration: '0.6s'
 	};
 
+	//con, mask의 활성화 스타일 객체
 	const conStyleActive = { ...conStyle, opacity: 1 };
 	const maskStyleActive = { ...maskStyle, left: '100%' };
 
 	useEffect(() => {
 		setTimeout(() => {
 			setMounted(true);
-		}, 100);
+		}, 1000);
 	}, []);
 
 	return (
-		<div className='slogan' style={frameStyle} ref={ref_frame}>
+		<div className='slogan' style={frameStyle}>
 			<span style={Mounted ? conStyleActive : conStyle}>{children}</span>
-			<div className='mask' style={Mounted ? maskStyleActive : maskStyle}></div>
+			<div style={Mounted ? maskStyleActive : maskStyle}></div>
 		</div>
 	);
 }
