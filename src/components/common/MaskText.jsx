3d27c6c48cc5444936bfe9a43a7f5c8e19da5afe
mania605 <kim.minsun83@gmail.com>
2024-10-08 15:18:54 +0900

    setTimouet으로 0.1초동안 리얼돔 변환되는 시간 확보후 on클래스 추가

---
+++ src/components/common/MaskText.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from 'react';
+export default function MaskText({ children }) {
+	const ref_frame = useRef(null);
+	useEffect(() => {
+		setTimeout(() => {
+			ref_frame.current.classList.add('on');
+		}, 100);
+	}, []);
+	return (
+		<div className='slogan' ref={ref_frame}>
+			<span>{children}</span>
+			<div className='mask'></div>
+		</div>
+	);
+}
