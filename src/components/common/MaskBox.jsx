b655f76618a939073f940c5efc4afee9ec34cf40
mania605 <kim.minsun83@gmail.com>
2024-10-08 17:32:05 +0900

    maskBox컴포넌트 추가

--- src/components/common/MaskBox.jsx
+++ src/components/common/MaskBox.jsx
@@ -1,47 +1,40 @@
 import { motion } from 'framer-motion';
 
-export default function MaskBox({
-	children,
-	duration,
-	delay,
-	color,
-	style,
-	maskColor = '#000'
-}) {
-	// 기본 스타일 객체
+export default function MaskBox({ children, duration, delay, color, style }) {
+	//기본 스타일 객체
 	const frameStyle = {
 		display: 'inline-block',
 		position: 'relative',
 		overflow: 'hidden',
-		width: '100%',
-		height: '100%',
-		...style
+		marginBottom: 20
 	};
-
 	const maskStyle = {
 		width: '100%',
 		height: '100%',
 		position: 'absolute',
 		top: 0,
-		left: 0,
-		backgroundColor: maskColor
+		backgroundColor: color
 	};
 
 	return (
-		<div style={frameStyle}>
-			<motion.div
-				style={maskStyle}
-				initial={{ x: '-100%' }}
-				animate={{ x: '100%' }}
-				transition={{ duration, delay }}></motion.div>
-			{/* 마스크가 사라지면서 자식 요소가 보여짐 */}
-			<motion.div
+		<div style={{ ...frameStyle, ...style }}>
+			<motion.span
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
-				exit={{ opacity: 0 }}
+				exit={{ opacity: 0, transition: { delay: 0 } }}
 				transition={{ duration: 0.01, delay: duration / 2 + delay }}>
 				{children}
-			</motion.div>
+			</motion.span>
+
+			<motion.div
+				style={maskStyle}
+				initial={{ x: '-101%' }}
+				animate={{ x: '101%' }}
+				transition={{ duration, delay }}></motion.div>
 		</div>
 	);
 }
+
+//미션
+//MaskBox.jsx라는 새로운 컴포넌트 생성
+//이미지나 그룹덩어리의 박스요소에 마스크 모션 처리
